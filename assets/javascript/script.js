939ac1a2f358aeb9ad3071a4b38bccf9b11bf6fb
Hustin <hustinkava@gmail.com>
2020-10-20 11:06:53 -0600

    code update

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -21,7 +21,7 @@ var passwordValidation = (userInput) => {
 // Variables section that contain arrays with all the characters needed.
 let lowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let upCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-let numCase = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
+let numCase = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let specCase = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 
 // Created a function named userConfirmationInput that passes passwordLength as a parameter.
@@ -39,60 +39,34 @@ var userConfirmationInput = (passwordLength) => {
 
   // Initialzed another empty array like we're doing with password. Reason why is if the array is inside of the if statement, it cannot be accessed due to scope.
   let randomSelectionArray = []
+    
 
-  // If no option is selected
-  if (!isLow && !isUp && !isNum && !isSpec) {
-    alert('You must select at least one option')
-    return 'Please try again';
+    if (isLow) {
+      randomSelectionArray = randomSelectionArray.concat(lowCase)
+    } 
 
-    // The purpose of an empty array is for the storage of letters, numbers or special characters. 
-    // The lines below will do a check for what options have been selected.
-  } else if (isLow && isUp && isNum && isSpec) {
-    // This line below will add the array of lowCase, upCase, numCase and specCase to the randomSelectionArray. 
-    // Concat will join and add the users selected arrays to the randomSelectionArray.
-    randomSelectionArray = randomSelectionArray.concat(lowCase, upCase, numCase, specCase)
+    if (isUp) {
+      randomSelectionArray = randomSelectionArray.concat(upCase)
+    } 
+
+    if (isNum) {
+      randomSelectionArray = randomSelectionArray.concat(numCase)
+    } 
+
+    if (isSpec) {
+      randomSelectionArray = randomSelectionArray.concat(specCase)
+    } 
+
+    if (!isLow && !isUp && !isNum && !isSpec) {
+      alert('You must select at least one option')
+    }
+
     console.log(randomSelectionArray);
-
-    //Three options true.
-  } else if (isLow && isUp && isNum) {
-    randomSelectionArray = randomSelectionArray.concat(lowCase, upCase, numCase);
-  } else if (isLow && isUp && isSpec) {
-    randomSelectionArray = randomSelectionArray.concat(lowCase, upCase, specCase);
-  } else if (isLow && isNum && isSpec) {
-    randomSelectionArray = randomSelectionArray.concat(lowCase, numCase, specCase);
-  } else if (isUp && isNum && isSpec) {
-    randomSelectionArray = randomSelectionArray.concat(upCase, numCase, specCase);
-
-
-    //Two options true.
-  } else if (isLow && isUp) {
-    randomSelectionArray = randomSelectionArray.concat(lowCase, upCase);
-  } else if (isLow && isNum) {
-    randomSelectionArray = randomSelectionArray.concat(lowCase, numCase);
-  } else if (isLow && isSpec) {
-    randomSelectionArray = randomSelectionArray.concat(lowCase, specCase);
-  } else if (isUp && isSpec) {
-    randomSelectionArray = randomSelectionArray.concat(upCase, specCase);
-  } else if (isUp && isSpec) {
-    randomSelectionArray = randomSelectionArray.concat(upCase, specCase);
-  } else if (isNum && isSpec) {
-    randomSelectionArray = randomSelectionArray.concat(numCase, specCase);
-
-
-    //One option true.
-  } else if (isLow) {
-    randomSelectionArray = randomSelectionArray.concat(lowCase);
-  } else if (isUp) {
-    randomSelectionArray = randomSelectionArray.concat(upCase);
-  } else if (isNum) {
-    randomSelectionArray = randomSelectionArray.concat(numCase);
-  } else if (isSpec) {
-    randomSelectionArray = randomSelectionArray.concat(specCase);
-  }
-
+    
+  
   // For loop below will be based on the length that the user wants the password to be. 
   for (var i = 0; i < passwordLength; i++) {
-    // RandomCharacter is the variavle that stores the output of the functions below.
+    // RandomCharacter is the variable that stores the output of the functions below.
     let randomCharacter = Math.floor(Math.random() * randomSelectionArray.length);
     // This will randomly select X amount of times (what password Length is) the position of the randomCharacter in the randomSelectionArray.
     // passwordArray.push will add the position of randomCharacter in the randomSelectionArray to the passwordArray.
@@ -111,6 +85,7 @@ var generatePassword = () => {
   } else {
     console.log('no');
   }
+
 
 };
 // Write password to the #password input
